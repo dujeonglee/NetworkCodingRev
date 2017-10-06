c736e815caaa2c1f532a228536010b099e83c9c1
Dujeong Lee <dujeong.lee82@gmail.com>
2017-10-06 17:37:29 +0900

    Provide example codes for using dynamic and static ncsocket libraries.
    Provide example codes for using dynamic and static ncsocket libraries.

--- Makefile
+++ Makefile
@@ -2,20 +2,38 @@
 .SUFFIXES : .cpp .o
 
 INCLUDES := -I./basiclibrary/avltree -I./basiclibrary/threadpool -I./basiclibrary/singleshottimer
+OPTIMIZATIONCPPFLAGS := -O3
+BASICCPPFLAGS := -c -Wall -std=c++0x
 CPP := g++
 
-./basiclibrary/.git/config : 
-	git clone https://github.com/dujeonglee/basiclibrary.git
+all : release
 
-all : debug
+dynamic : run_dynamic
+
+static : run_static
+
+run_static : main_stc.o libncsocket.a
+	$(CROSS)$(CPP) -o $@ main_stc.o ./libncsocket.a -lpthread
+run_dynamic : main_dyn.o libncsocket.so
+	$(CROSS)$(CPP) -o $@ main_dyn.o -lpthread -ldl
+main_stc.o : main.cpp
+	$(CROSS)$(CPP) -o $@ main.cpp $(OPTIMIZATIONCPPFLAGS) $(BASICCPPFLAGS) $(INCLUDES)
+main_dyn.o : main.cpp
+	$(CROSS)$(CPP) -o $@ main.cpp -DDYNAMIC $(OPTIMIZATIONCPPFLAGS) $(BASICCPPFLAGS) $(INCLUDES)
 
 clean :
-	rm -rf *.o *.so *.a *~ gmon.out *.bak *.stackdump run ./out
+	rm -rf *.o *.so *.a *~ gmon.out *.bak *.stackdump run_* ./out
 
-SHAREDLIBCPPFLAGS := -fPIC -O3 -c -Wall -std=c++0x
+basiclibrary/.git/config : 
+	git clone https://github.com/dujeonglee/basiclibrary.git
 SHAREDLIBOUT := out/shared
+sharedlibrary : libncsocket.so
+libncsocket.so : $(SHAREDLIBOUT) basiclibrary/.git/config $(SHAREDLIBOUT)/finite_field.o $(SHAREDLIBOUT)/tx.o $(SHAREDLIBOUT)/rx.o $(SHAREDLIBOUT)/c_stub.o
+	$(CROSS)$(CPP) -shared -o $@ $(SHAREDLIBOUT)/finite_field.o $(SHAREDLIBOUT)/tx.o $(SHAREDLIBOUT)/rx.o $(SHAREDLIBOUT)/c_stub.o
 $(SHAREDLIBOUT) :
 	mkdir -p $(SHAREDLIBOUT)
+SHAREDLIBCPPFLAGS := -fPIC -O3 $(BASICCPPFLAGS)
+SHAREDLIBOUT := out/shared
 $(SHAREDLIBOUT)/finite_field.o : finite_field.h finite_field.cpp
 	$(CROSS)$(CPP) finite_field.cpp -o $@ $(SHAREDLIBCPPFLAGS) $(INCLUDES)
 $(SHAREDLIBOUT)/tx.o : tx.h tx.cpp
@@ -24,14 +42,15 @@ $(SHAREDLIBOUT)/rx.o : rx.h rx.cpp
 	$(CROSS)$(CPP) rx.cpp -o $@ $(SHAREDLIBCPPFLAGS) $(INCLUDES)
 $(SHAREDLIBOUT)/c_stub.o : c_stub.h c_stub.cpp
 	$(CROSS)$(CPP) c_stub.cpp -o $@ $(SHAREDLIBCPPFLAGS) $(INCLUDES)
-libncsocket.so : $(SHAREDLIBOUT) $(SHAREDLIBOUT)/finite_field.o $(SHAREDLIBOUT)/tx.o $(SHAREDLIBOUT)/rx.o $(SHAREDLIBOUT)/c_stub.o
-	$(CROSS)$(CPP) -shared -o $@ $(SHAREDLIBOUT)/finite_field.o $(SHAREDLIBOUT)/tx.o $(SHAREDLIBOUT)/rx.o $(SHAREDLIBOUT)/c_stub.o
-sharedlibrary : libncsocket.so
 
-STATICLIBCPPFLAGS := -O3 -c -Wall -std=c++0x
 STATICLIBOUT := out/static
+staticlibrary : libncsocket.a
+libncsocket.a : $(STATICLIBOUT) basiclibrary/.git/config $(STATICLIBOUT)/finite_field.o $(STATICLIBOUT)/tx.o $(STATICLIBOUT)/rx.o $(STATICLIBOUT)/c_stub.o
+	$(CROSS)$(AR) rcs $@ $(STATICLIBOUT)/finite_field.o $(STATICLIBOUT)/tx.o $(STATICLIBOUT)/rx.o $(STATICLIBOUT)/c_stub.o
 $(STATICLIBOUT) :
 	mkdir -p $(STATICLIBOUT)
+STATICLIBCPPFLAGS := -O3 $(BASICCPPFLAGS)
+STATICLIBOUT := out/static
 $(STATICLIBOUT)/finite_field.o : finite_field.h finite_field.cpp
 	$(CROSS)$(CPP) finite_field.cpp -o $@ $(STATICLIBCPPFLAGS) $(INCLUDES)
 $(STATICLIBOUT)/tx.o : tx.h tx.cpp
@@ -40,20 +59,3 @@ $(STATICLIBOUT)/rx.o : rx.h rx.cpp
 	$(CROSS)$(CPP) rx.cpp -o $@ $(STATICLIBCPPFLAGS) $(INCLUDES)
 $(STATICLIBOUT)/c_stub.o : c_stub.h c_stub.cpp
 	$(CROSS)$(CPP) c_stub.cpp -o $@ $(STATICLIBCPPFLAGS) $(INCLUDES)
-libncsocket.a : $(STATICLIBOUT) $(STATICLIBOUT)/finite_field.o $(STATICLIBOUT)/tx.o $(STATICLIBOUT)/rx.o $(STATICLIBOUT)/c_stub.o
-	$(CROSS)$(AR) rcs $@ $(STATICLIBOUT)/finite_field.o $(STATICLIBOUT)/tx.o $(STATICLIBOUT)/rx.o $(STATICLIBOUT)/c_stub.o
-staticlibrary : libncsocket.a
-
-debug : CPPFLAGS := -g -c -Wall -std=c++0x
-debug : ./basiclibrary/.git/config
-debug : run
-
-release : CPPFLAGS := -O3 -c -Wall -std=c++0x
-release : ./basiclibrary/.git/config
-release : run
-
-main.o : main.cpp
-	$(CROSS)$(CPP) -o $@ main.cpp $(CPPFLAGS) $(INCLUDES)
-
-run : main.o libncsocket.so libncsocket.a
-	$(CROSS)$(CPP) -o $@ main.o ./libncsocket.a -lpthread -ldl
--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <iostream>
 #include "c_stub.h"
+#ifdef DYNAMIC
+#include <dlfcn.h>
+#endif
+
 using namespace std;
 
 bool Done = false;
@@ -43,12 +47,16 @@ int main(int argc, char *argv[])
 {
     if (argc == 2)
     {
-        char *local_port;
-
+        const char *const local_port = argv[1];
         std::cout << "Recv Mode" << std::endl;
-        local_port = argv[1];
-
+#ifndef DYNAMIC
+        std::cout << "Static" << std::endl;
         void *handle = InitSocket(local_port, 500, 500, RxCallback);
+#else
+        std::cout << "Dynamic" << std::endl;
+        void *pLibHandle = dlopen("./libncsocket.so", RTLD_LAZY);
+        void *handle = ((InitSocketFuncType)dlsym(pLibHandle, "InitSocket"))(local_port, 500, 500, RxCallback);
+#endif
         std::thread bwchk = std::thread(BandwidthCheck);
         bwchk.detach();
         while (Done == false)
@@ -56,43 +64,69 @@ int main(int argc, char *argv[])
             std::this_thread::sleep_for(std::chrono::milliseconds(1));
         }
         fclose(p_File);
+#ifndef DYNAMIC
         FreeSocket(handle);
+#else
+        ((FreeSocketFuncType)dlsym(pLibHandle, "FreeSocket"))(handle);
+        dlclose(pLibHandle);
+#endif
     }
     else if (argc == 5)
     {
-        char *local_port = nullptr;
-        char *remote_ip = nullptr;
-        char *remote_port = nullptr;
+        const char *const local_port = argv[1];
+        const char *const remote_ip = argv[2];
+        const char *const remote_port = argv[3];
         unsigned char buffer[1424];
         size_t readbytes;
         FILE *p_File = nullptr;
         std::cout << "Send Mode" << std::endl;
-        local_port = argv[1];
-        remote_ip = argv[2];
-        remote_port = argv[3];
         p_File = fopen(argv[4], "r");
         if (p_File == nullptr)
         {
             std::cout << "Cannot open file " << argv[4] << std::endl;
             return -1;
         }
-
+#ifndef DYNAMIC
+        std::cout << "Static" << std::endl;
         void *handle = InitSocket(local_port, 500, 500, nullptr);
-        while (false == Connect(handle, remote_ip, remote_port, 1000,
-                                NetworkCoding::Parameter::RELIABLE_TRANSMISSION_MODE,
-                                NetworkCoding::Parameter::BLOCK_SIZE_32, (uint16_t)0))
-            ;
+        do
+        {
+
+        } while (false == Connect(handle, remote_ip, remote_port, 1000,
+                                  NetworkCoding::Parameter::RELIABLE_TRANSMISSION_MODE,
+                                  NetworkCoding::Parameter::BLOCK_SIZE_32, (uint16_t)0));
         Send(handle, remote_ip, remote_port, (unsigned char *)argv[4], strlen(argv[4]));
         while ((readbytes = fread(buffer, 1, sizeof(buffer), p_File)) > 0)
         {
             Send(handle, remote_ip, remote_port, buffer, readbytes);
         }
-
         buffer[0] = 0xff;
         Send(handle, remote_ip, remote_port, buffer, 1);
         WaitUntilTxIsCompleted(handle, remote_ip, remote_port);
         fclose(p_File);
         FreeSocket(handle);
+#else
+        std::cout << "Dynamic" << std::endl;
+        void *pLibHandle = dlopen("./libncsocket.so", RTLD_LAZY);
+        void *handle = ((InitSocketFuncType)dlsym(pLibHandle, "InitSocket"))(local_port, 500, 500, RxCallback);
+        do
+        {
+
+        } while (false == ((ConnectFuncType)dlsym(pLibHandle, "Connect"))(handle, remote_ip, remote_port, 1000,
+                                                                          NetworkCoding::Parameter::RELIABLE_TRANSMISSION_MODE,
+                                                                          NetworkCoding::Parameter::BLOCK_SIZE_32, (uint16_t)0));
+        ((SendFuncType)dlsym(pLibHandle, "Send"))(handle, remote_ip, remote_port, (unsigned char *)argv[4], strlen(argv[4]));
+        while ((readbytes = fread(buffer, 1, sizeof(buffer), p_File)) > 0)
+        {
+            ((SendFuncType)dlsym(pLibHandle, "Send"))(handle, remote_ip, remote_port, buffer, readbytes);
+        }
+        buffer[0] = 0xff;
+        ((SendFuncType)dlsym(pLibHandle, "Send"))(handle, remote_ip, remote_port, buffer, 1);
+        ((WaitUntilTxIsCompletedFuncType)dlsym(pLibHandle, "WaitUntilTxIsCompleted"))(handle, remote_ip, remote_port);
+        fclose(p_File);
+        ((FreeSocketFuncType)dlsym(pLibHandle, "FreeSocket"))(handle);
+        dlclose(pLibHandle);
+#endif
     }
     else
     {
