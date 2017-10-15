3696e6208499586041089540843e9760e538f8c7
Dujeong Lee <dujeong.lee82@gmail.com>
2017-10-15 23:00:13 +0900

    Example is updated. Demonstrate sychronous receiving and asynchronous receiving using callback.

--- example/c_cpp/main.cpp
+++ example/c_cpp/main.cpp
@@ -8,28 +8,27 @@
 
 using namespace std;
 
-bool Done = false;
+#define USE_RX_CALLBACK (0)
 
 int main(int argc, char *argv[])
 {
     if (argc == 2)
     {
         std::cout << "Recv Mode" << std::endl;
-        void *handle = InitSocket(argv[1], 500, 500, nullptr);
-
         FILE *p_File = nullptr;
+        float mbyte_received = 0;
+        float total_mbyte_received = 0;
+        uint32_t samples = 0;
+#if !USE_RX_CALLBACK
         uint8_t buffer[1500];
         uint16_t buffer_length = sizeof(buffer);
         union {
             sockaddr_in ip4;
             sockaddr_in6 ip6;
         } addr;
-        uint32_t addr_length;
+        uint32_t addr_length = sizeof(addr);
 
-        float mbyte_received = 0;
-        float total_mbyte_received = 0;
-        uint32_t samples = 0;
-
+        void *handle = InitSocket(argv[1], 500, 500, nullptr);
         const bool ret = Receive(handle, buffer, &buffer_length, &addr, &addr_length, 0);
         if (ret)
         {
@@ -63,7 +62,7 @@ int main(int argc, char *argv[])
             addr_length = sizeof(addr);
             if (Receive(handle, buffer, &buffer_length, &addr, &addr_length, 0))
             {
-                if(!(buffer_length == 1 && buffer[0] == 0xff))
+                if (!(buffer_length == 1 && buffer[0] == 0xff))
                 {
                     fwrite(buffer, 1, buffer_length, p_File);
                     mbyte_received += ((float)buffer_length / (1000000.));
@@ -75,6 +74,52 @@ int main(int argc, char *argv[])
         p_File = nullptr;
         FreeSocket(handle);
         handle = nullptr;
+#else
+        bool done = false;
+        void *handle = InitSocket(
+            argv[1],
+            500,
+            500,
+            [&p_File, &total_mbyte_received, &mbyte_received, &samples, &done](uint8_t *const buffer, const uint16_t length, const void *const address, const uint32_t sender_addr_len) {
+                if (p_File == nullptr)
+                {
+                    buffer[length] = 0;
+                    p_File = fopen((char *)buffer, "w");
+                    std::thread bwchk = std::thread([&total_mbyte_received, &mbyte_received, &samples, &done]() {
+                        while (!done)
+                        {
+                            std::this_thread::sleep_for(std::chrono::seconds(1));
+                            total_mbyte_received += mbyte_received;
+                            mbyte_received = 0;
+                            samples++;
+                            printf("[AVG %5.5f MB/s][%u seconds]\n", total_mbyte_received / samples, samples);
+                            fflush(stdout);
+                        }
+                    });
+                    bwchk.detach();
+                }
+                else
+                {
+                    if (!(length == 1 && buffer[0] == 0xff))
+                    {
+                        fwrite(buffer, 1, length, p_File);
+                        mbyte_received += ((float)length / (1000000.));
+                    }
+                    else
+                    {
+                        done = true;
+                    }
+                }
+            });
+        while (!done)
+        {
+            std::this_thread::sleep_for(std::chrono::milliseconds(500));
+        }
+        fclose(p_File);
+        p_File = nullptr;
+        FreeSocket(handle);
+        handle = nullptr;
+#endif
     }
     else if (argc == 5)
     {
@@ -92,7 +137,7 @@ int main(int argc, char *argv[])
             std::cout << "Cannot open file " << argv[4] << std::endl;
             exit(-1);
         }
-        
+
         unsigned char buffer[1424];
         size_t readbytes;
         Send(handle, argv[2], argv[3], (unsigned char *)argv[4], strlen(argv[4]));
